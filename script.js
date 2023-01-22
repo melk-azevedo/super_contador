93dc0dd23a27a3cf68cb8ed1161dac0d87b5c597
Melk Azevedo <melk.azevedo@outlook.com>
2023-01-22 09:08:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function contar() {
     let inc = document.getElementById("txti")
-    let fim = document.querySelector("input#txtf")
-    let passo = document.querySelector("input#txtp")
-    let res = document.getElementById("res")
+        fim = document.querySelector("input#txtf")
+        passo = document.querySelector("input#txtp")
+        res = document.getElementById("res")
 
     if (inc.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         res.innerHTML = "Impossivel contar!"
@@ -10,8 +10,8 @@ function contar() {
     } else {
         res.innerHTML = `Contando`
         let i = Number(inc.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
+            f = Number(fim.value)
+            p = Number(passo.value)
 
         if (p <= 0) {
             window.alert("Passo inválido! Considerando PASSO 1")
